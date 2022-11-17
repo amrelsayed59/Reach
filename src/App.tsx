526524089165457cabb2b4863383eb07ea2bbcff
Amr Ahmed <amr.ahmed@safavisa.com>
2022-11-17 19:14:03 +0200

    init app

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Routes from './routes';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
+const App: React.FC = () => {
+  return (
+    <>
+      <Routes />
+    </>
+  );
+};
+
+export default App;
---
+++ src/routes.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { BrowserRouter, Switch } from "react-router-dom";
+
+
+const Routes: React.SFC = () => (
+  <BrowserRouter>
+    <Switch></Switch>
+  </BrowserRouter>
+);
+
+export default Routes;
